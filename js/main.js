4d8923709ecacff97d5976c3ef8df78db028f9fb
MD-Nasir301 <nasir21b@gmail.com>
2020-09-04 21:00:26 +0600

    almost done

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 (function ($) {
 "use strict";
 
-// meanmenu--------------------
-// $('#mobile-menu').meanmenu({
-// 	meanMenuContainer: '.mobile-menu',
-// 	meanScreenWidth: "992"
-// });
-
-
-
 // Sticky nav ------------------
 window.addEventListener("scroll", function(){
-    var nav = document.querySelector('nav');
+    var nav = document.querySelector('.main-menu-content');
     nav.classList.toggle("sticky-nav", window.scrollY > 100);
 });
-      
+
+// Nav background set
+$(window).on("resize",function() {
+    if ($(window).width() < 992) {
+        var nav = document.querySelector('.main-menu-content');
+        nav.classList.add("nav-bg");
+    }
+   else {
+    var nav = document.querySelector('.main-menu-content');
+      nav.classList.remove("nav-bg");
+   }
+  });
 
 // ityped -------------------
 ityped.init(document.querySelector("#ityped"), {
@@ -33,57 +36,6 @@ ityped.init(document.querySelector("#ityped"), {
     delay: 10,
     time: 1500
 });
-// Scroll Top----------------
-// $(window).on('scroll', function () {
-// 	var scroll = $(window).scrollTop();
-// 	if (scroll < 245) {
-// 		$(".header-sticky").removeClass("sticky");
-// 	} else {
-// 		$(".header-sticky").addClass("sticky");
-// 	}
-// });
-
-
-// mainSlider----------------
-// function mainSlider() {
-// 	var BasicSlider = $('.slider-active');
-// 	BasicSlider.on('init', function (e, slick) {
-// 		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-// 		doAnimations($firstAnimatingElements);
-// 	});
-// 	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-// 		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-// 		doAnimations($animatingElements);
-// 	});
-// 	BasicSlider.slick({
-// 		autoplay: false,
-// 		autoplaySpeed: 10000,
-// 		dots: false,
-// 		fade: true,
-// 		arrows: false,
-// 		responsive: [
-// 			{ breakpoint: 767, settings: { dots: false, arrows: false } }
-// 		]
-// 	});
-
-// 	function doAnimations(elements) {
-// 		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-// 		elements.each(function () {
-// 			var $this = $(this);
-// 			var $animationDelay = $this.data('delay');
-// 			var $animationType = 'animated ' + $this.data('animation');
-// 			$this.css({
-// 				'animation-delay': $animationDelay,
-// 				'-webkit-animation-delay': $animationDelay
-// 			});
-// 			$this.addClass($animationType).one(animationEndEvents, function () {
-// 				$this.removeClass($animationType);
-// 			});
-// 		});
-// 	}
-// }
-// mainSlider();
-
 
 // owlCarousel------------------------
 $(".owl-carousel-choose").owlCarousel({
@@ -139,6 +91,7 @@ $(".owl-carousel-investment-plan").owlCarousel({
    
 });
 
+// slick-slider
 $('.testimonial-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -170,74 +123,35 @@ $(document).ready(function() {
 });
 
 
+// calculate Profit
 
-// magnificPopup video view---------------------
-// $('.popup-video').magnificPopup({
-// 	type: 'iframe'
-// });
+$('.investment-input-field input').on("input",function(e){
+   var investAmount =parseInt($(this).val());
+   var investPer = parseInt($('.investment-input-field select').val());
+   var dailyProfit=((investAmount*investPer/100)*1).toFixed(2)
+   var weeklyProfit= ((investAmount*investPer/100)*7).toFixed(2)   
+   var monthlyProfit=((investAmount*investPer/100)*30).toFixed(2)
+   $('.daily-profit span').html(dailyProfit)
+   $('.weekly-profit span').html(weeklyProfit)
+   $('.monthly-profit span').html(monthlyProfit)
 
+   if(e.target.value == ''){
+        $('.daily-profit span').html(0)
+        $('.weekly-profit span').html(0)
+        $('.monthly-profit span').html(0)
+   }
+ }) 
 
-// isotop--------------------
-// $('.grid').imagesLoaded( function() {
-// 	// init Isotope
-// 	var $grid = $('.grid').isotope({
-// 	  itemSelector: '.grid-item',
-// 	  percentPosition: true,
-// 	  masonry: {
-// 		// use outer width of grid-sizer for columnWidth
-// 		columnWidth: '.grid-item',
-// 	  }
-// 	});
-// });
-
-
-// filter items on button click--------------
-// $('.portfolio-menu').on( 'click', 'button', function() {
-//   var filterValue = $(this).attr('data-filter');
-//   $grid.isotope({ filter: filterValue });
-// });
-
-
-//for menu active class-------------
-// $('.portfolio-menu button').on('click', function(event) {
-// 	$(this).siblings('.active').removeClass('active');
-// 	$(this).addClass('active');
-// 	event.preventDefault();
-// });
-
-
-// scrollToTop-----------
-// $.scrollUp({
-// 	scrollName: 'scrollUp', // Element ID
-// 	topDistance: '300', // Distance from top before showing element (px)
-// 	topSpeed: 300, // Speed back to top (ms)
-// 	animation: 'fade', // Fade, slide, none
-// 	animationInSpeed: 200, // Animation in speed (ms)
-// 	animationOutSpeed: 200, // Animation out speed (ms)
-// 	scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
-// 	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-// });
-
-// One Page Nav---------------
-// var top_offset = $('.header-area').height() -0;
-
-// $('.main-menu ul').onePageNav({
-//     currentClass: 'active',
-//     scrollSpeed: 2000,
-// 	scrollThreshold: 0,
-// 	scrollOffset: top_offset,
-// });
-// $('.main-menu ul').onePageNav({
-// 	currentClass: 'current',
-// 	changeHash: false,
-// 	scrollSpeed: 750,
-// 	scrollThreshold: 0.5,
-// 	easing: 'swing',
-// });
-
-
-// WOW active-------------------
-// new WOW().init(); 
+$('.investment-input-field select').on("input",function  (){
+    var investAmount =parseInt($('.investment-input-field input').val());
+    var investPer = parseInt($(this).val());
+    var dailyProfit=((investAmount*investPer/100)*1).toFixed(2)
+    var weeklyProfit= ((investAmount*investPer/100)*7).toFixed(2)   
+    var monthlyProfit=((investAmount*investPer/100)*30).toFixed(2)
+    $('.daily-profit span').html(dailyProfit)
+    $('.weekly-profit span').html(weeklyProfit)
+    $('.monthly-profit span').html(monthlyProfit)
+})
 
 
 })(jQuery);
