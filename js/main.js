190864ee15604f90e4e9cec222726e24dfe4d2bd
MD-Nasir301 <nasir21b@gmail.com>
2020-09-06 13:48:31 +0600

    css index organized done

--- js/main.js
+++ js/main.js
@@ -143,14 +143,22 @@ $('.investment-input-field input').on("input",function(e){
  }) 
 
 $('.investment-input-field select').on("input",function  (){
-    var investAmount =parseInt($('.investment-input-field input').val());
-    var investPer = parseInt($(this).val());
-    var dailyProfit=((investAmount*investPer/100)*1).toFixed(2)
-    var weeklyProfit= ((investAmount*investPer/100)*7).toFixed(2)   
-    var monthlyProfit=((investAmount*investPer/100)*30).toFixed(2)
-    $('.daily-profit span').html(dailyProfit)
-    $('.weekly-profit span').html(weeklyProfit)
-    $('.monthly-profit span').html(monthlyProfit)
+    if($('.investment-input-field input').val() == ''){
+        $('.daily-profit span').html(0)
+        $('.weekly-profit span').html(0)
+        $('.monthly-profit span').html(0)
+    }
+    else{
+        var investAmount =parseInt($('.investment-input-field input').val());
+        var investPer = parseInt($(this).val());
+        var dailyProfit=((investAmount*investPer/100)*1).toFixed(2)
+        var weeklyProfit= ((investAmount*investPer/100)*7).toFixed(2)   
+        var monthlyProfit=((investAmount*investPer/100)*30).toFixed(2)
+        $('.daily-profit span').html(dailyProfit)
+        $('.weekly-profit span').html(weeklyProfit)
+        $('.monthly-profit span').html(monthlyProfit)
+    }
+
 })
 
 
