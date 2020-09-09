e4b9c2db627059095938b1938b6b95321cfb41a7
MD-Nasir301 <nasir21b@gmail.com>
2020-09-10 00:17:06 +0600

    b

--- js/main.js
+++ js/main.js
@@ -2,126 +2,126 @@ $( document ).ready(function() {
 
 
 
-// Sticky nav ------------------
-window.addEventListener("scroll", function(){
-    var nav = document.querySelector('.main-menu-content');
-    nav.classList.toggle("sticky-nav", window.scrollY > 100);
-});
+// // Sticky nav ------------------
+// window.addEventListener("scroll", function(){
+//     var nav = document.querySelector('.main-menu-content');
+//     nav.classList.toggle("sticky-nav", window.scrollY > 100);
+// });
 
-// Nav background set
-$(window).on("resize",function() {
-    if ($(window).width() < 992) {
-        var nav = document.querySelector('.main-menu-content');
-        nav.classList.add("nav-bg");
-    }
-   else {
-    var nav = document.querySelector('.main-menu-content');
-      nav.classList.remove("nav-bg");
-   }
-  });
+// // Nav background set
+// $(window).on("resize",function() {
+//     if ($(window).width() < 992) {
+//         var nav = document.querySelector('.main-menu-content');
+//         nav.classList.add("nav-bg");
+//     }
+//    else {
+//     var nav = document.querySelector('.main-menu-content');
+//       nav.classList.remove("nav-bg");
+//    }
+//   });
 
-// ityped -------------------
-ityped.init(document.querySelector("#ityped"), {
-    showCursor: true,
-    strings: ['DEPOSIT', 'BETTING', 'CRYPTOCURRENCY', 'EARN MONEY ',' HIGH YIELD','HYIP','INVESTMENT ',' PROFIT','SPORTING','TOURNAMENT','MUCH MORE...'],
-    loop: true,
-    backSpeed: 50,
-    typeSpeed: 50,
-    cursorChar: "",
-    backDelay:  1100,
-  });
+// // ityped -------------------
+// ityped.init(document.querySelector("#ityped"), {
+//     showCursor: true,
+//     strings: ['DEPOSIT', 'BETTING', 'CRYPTOCURRENCY', 'EARN MONEY ',' HIGH YIELD','HYIP','INVESTMENT ',' PROFIT','SPORTING','TOURNAMENT','MUCH MORE...'],
+//     loop: true,
+//     backSpeed: 50,
+//     typeSpeed: 50,
+//     cursorChar: "",
+//     backDelay:  1100,
+//   });
 
 
-// counter ------------------
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1500
-});
+// // counter ------------------
+//   $('.counter').counterUp({
+//     delay: 10,
+//     time: 1500
+// });
 
-// owlCarousel------------------------
-$(".owl-carousel-choose").owlCarousel({
-    margin:30,
-    autoplay:true,
-    autoplayTimeout:2000,
-    nav:true,
-    dots:true,
-    navText:['<i class="icofont-thin-left"></i>','<i class="icofont-thin-right"></i>'],
-    responsive:{
-        0:{
-            items:1,
-        },
-        576:{
-            items:2,
-        },
-        768:{
-            items:3,
-        },
-        992:{
-            items:4,
-        },
-        1200:{
-            items:5,
-        }
+// // owlCarousel------------------------
+// $(".owl-carousel-choose").owlCarousel({
+//     margin:30,
+//     autoplay:true,
+//     autoplayTimeout:2000,
+//     nav:true,
+//     dots:true,
+//     navText:['<i class="icofont-thin-left"></i>','<i class="icofont-thin-right"></i>'],
+//     responsive:{
+//         0:{
+//             items:1,
+//         },
+//         576:{
+//             items:2,
+//         },
+//         768:{
+//             items:3,
+//         },
+//         992:{
+//             items:4,
+//         },
+//         1200:{
+//             items:5,
+//         }
 
-    }
+//     }
    
-});
+// });
 
-$(".owl-carousel-investment-plan").owlCarousel({
-    margin:30,
-    autoplay:true,
-    autoplayTimeout:200000000,
-    nav:true,
-    dots:true,
-    navText:['<i class="icofont-thin-left"></i>','<i class="icofont-thin-right"></i>'],
-    responsive:{
-        0:{
-            items:1,
-        },
-        576:{
-            items:1,
-        },
-        768:{
-            items:3,
-        },
-        1000:{
-            items:4,
-        }
+// $(".owl-carousel-investment-plan").owlCarousel({
+//     margin:30,
+//     autoplay:true,
+//     autoplayTimeout:200000000,
+//     nav:true,
+//     dots:true,
+//     navText:['<i class="icofont-thin-left"></i>','<i class="icofont-thin-right"></i>'],
+//     responsive:{
+//         0:{
+//             items:1,
+//         },
+//         576:{
+//             items:1,
+//         },
+//         768:{
+//             items:3,
+//         },
+//         1000:{
+//             items:4,
+//         }
 
-    }
+//     }
    
-});
+// });
 
-// slick-slider
-$('.testimonial-slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: false,
-    asNavFor: '.testimonial-thumb'
-  });
+// // slick-slider
+// $('.testimonial-slider').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: false,
+//     asNavFor: '.testimonial-thumb'
+//   });
 
-  $('.testimonial-thumb').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    asNavFor: '.testimonial-slider',
-    dots: false,
-    centerMode: false,
-    focusOnSelect: true
-  });
+//   $('.testimonial-thumb').slick({
+//     slidesToShow: 6,
+//     slidesToScroll: 1,
+//     asNavFor: '.testimonial-slider',
+//     dots: false,
+//     centerMode: false,
+//     focusOnSelect: true
+//   });
 
-// magnificPopup img view -----------------
-$(document).ready(function() {
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
+// // magnificPopup img view -----------------
+// $(document).ready(function() {
+//     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+//         disableOn: 700,
+//         type: 'iframe',
+//         mainClass: 'mfp-fade',
+//         removalDelay: 160,
+//         preloader: false,
 
-        fixedContentPos: false
-    });
-});
+//         fixedContentPos: false
+//     });
+// });
 
 
 // calculate Profit
