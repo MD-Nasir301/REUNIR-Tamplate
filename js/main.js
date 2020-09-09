fe14557e375374875841b3b909f23c2dd0435e4c
MD-Nasir301 <nasir21b@gmail.com>
2020-09-10 00:26:39 +0600

    f

--- js/main.js
+++ js/main.js
@@ -110,18 +110,18 @@ $('.testimonial-slider').slick({
     focusOnSelect: true
   });
 
-// // magnificPopup img view -----------------
-// $(document).ready(function() {
-//     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-//         disableOn: 700,
-//         type: 'iframe',
-//         mainClass: 'mfp-fade',
-//         removalDelay: 160,
-//         preloader: false,
+// magnificPopup img view -----------------
+$(document).ready(function() {
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
 
-//         fixedContentPos: false
-//     });
-// });
+        fixedContentPos: false
+    });
+});
 
 
 // calculate Profit
